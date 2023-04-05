4c58a9d3ae1ed9f282d24b7a123a6b04b4481c9a
Artsiom-Mazurkevich <patrol1995@yandex.ru>
2023-04-05 13:14:54 +0300

    fix login & me methods

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -30,7 +30,6 @@ class AuthController {
             const hashedPassword = await bcrypt.hash(password, salt)
 
             const newUser = await query_transaction_create_user(name, surname, email, hashedPassword, role_id)
-            console.log('NewUser: ', newUser)
 
             const token = generateToken(newUser.rows[0].id)
 
@@ -45,8 +44,10 @@ class AuthController {
             const { email, password } = req.body
 
             // Получаем информацию о пользователе из БД
-            // const result = await pool.query('SELECT * FROM users WHERE email = $1', [email])
-            const result = await make_request_to_db('SELECT * FROM users WHERE email = $1', [email])
+            const result = await make_request_to_db(
+                'SELECT Users.*, Passwords.password FROM users JOIN Passwords ON Users.password_id = Passwords.id WHERE Users.email = $1',
+                [email],
+            )
 
             if (result.rows.length === 0) {
                 return res.status(400).json({ message: 'Invalid credentials' })
@@ -90,9 +91,8 @@ class AuthController {
                 return res.status(404).json({ message: 'User not found' })
             }
 
-            const { id, name, surname, email, role } = result.rows[0]
-            // ❌❌❌❌❌❌❌❌❌ role
-            res.status(200).json({ id, name, surname, email, role })
+            const { id, name, surname, email } = result.rows[0]
+            res.status(200).json({ id, name, surname, email })
         } catch (err) {
             console.error(err)
             res.status(500).json({ message: 'Server error' })
